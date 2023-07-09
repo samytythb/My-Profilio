4f6f317f26d5fa79ae4c19f93ffd27bfa01af85d
Le Minh Duc <79952214+samytythb@users.noreply.github.com>
2023-07-09 23:59:58 +0700

    update

--- index.js
+++ index.js
@@ -19,3 +19,26 @@ document.getElementById("phone").addEventListener("click", function () {
   var phoneNumber = "+84778569006";
   window.location.href = "tel:" + phoneNumber;
 });
+var images = document.querySelectorAll(".full-screen-image");
+
+// Gắn kết sự kiện "click" cho từng thẻ ảnh
+images.forEach(function (image) {
+  image.addEventListener("click", function () {
+    // Lấy đường dẫn ảnh từ thẻ được nhấp
+    var imageURL = this.src;
+
+    // Hiển thị overlay và ảnh fullscreen
+    var overlay = document.querySelector(".fullscreen-overlay");
+    var fullscreenImage = overlay.querySelector(".fullscreen-image");
+    overlay.style.display = "block";
+    fullscreenImage.src = imageURL;
+
+    // Gắn kết sự kiện "click" cho nút đóng
+    var closeButton = overlay.querySelector(".close-button");
+    closeButton.addEventListener("click", function () {
+      // Đóng overlay và đặt lại đường dẫn ảnh về trống
+      overlay.style.display = "none";
+      fullscreenImage.src = "";
+    });
+  });
+});
